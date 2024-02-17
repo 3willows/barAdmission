d20c91512c8efc212f0d2766d8ca2369aeb07416
Jasper Wong <wlcjasper@gmail.com>
2024-02-17 22:08:48 +0800

    remove router

--- src/index.js
+++ src/index.js
@@ -1,46 +1,46 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
-import { createHashRouter, RouterProvider } from "react-router-dom"
+// import { createHashRouter, RouterProvider } from "react-router-dom"
 
 import App from "./routes/App"
-import NoticeOfMotion from "./routes/noticeOfMotion"
-import AffidavitOfApplicant from "./routes/affidavitOfApplicant"
-import AffidavitOfIdentity from "./routes/affidavitOfIdentity"
-import LetterTo3Regulators from "./routes/letterTo3Regulators"
+// import NoticeOfMotion from "./routes/noticeOfMotion"
+// import AffidavitOfApplicant from "./routes/affidavitOfApplicant"
+// import AffidavitOfIdentity from "./routes/affidavitOfIdentity"
+// import LetterTo3Regulators from "./routes/letterTo3Regulators"
 
 import ErrorPage from "./error-page"
 import { ContextProvider } from "./Context.js"
 
-const router = createHashRouter([
-  {
-    path: "/",
-    element: <App/>,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "notice",
-    element: <NoticeOfMotion />,
-  },
-  {
-    path: "applicant",
-    element: <AffidavitOfApplicant />,
-  },
-  {
-    path: "id",
-    element: <AffidavitOfIdentity />,
-  },
-  {
-    path: "letters",
-    element: <LetterTo3Regulators />,
-  },
-])
+// const router = createHashRouter([
+//   {
+//     path: "/",
+//     element: <App/>,
+//     errorElement: <ErrorPage />,
+//   },
+//   {
+//     path: "notice",
+//     element: <NoticeOfMotion />,
+//   },
+//   {
+//     path: "applicant",
+//     element: <AffidavitOfApplicant />,
+//   },
+//   {
+//     path: "id",
+//     element: <AffidavitOfIdentity />,
+//   },
+//   {
+//     path: "letters",
+//     element: <LetterTo3Regulators />,
+//   },
+// ])
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
     <ContextProvider>
-      <RouterProvider router={router} />
+      <App/>
     </ContextProvider>
   </React.StrictMode>
 )
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { Link } from "react-router-dom"
+// import { Link } from "react-router-dom"
 import "../App.css"
 import { useAppContext } from "../Context.js"
 import { DataInput } from "../components/DataInput"
