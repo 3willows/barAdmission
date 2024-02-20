a03e339298762bec2a2ccee4df74bc2e91f28de5
Jasper Wong <wlcjasper@gmail.com>
2024-02-20 21:26:48 +0800

    add page break

--- src/routes/coverLetters.jsx
+++ src/routes/coverLetters.jsx
@@ -4,7 +4,7 @@ export default function CoverLetters() {
   return (
     <div>
       <div className="m-5">
-        <div className="">
+        <div>
           <p>The Registrar</p>
           <p>High Court</p>
           <p>38 Queensway</p>
@@ -14,22 +14,27 @@ export default function CoverLetters() {
       </div>
 
       <div className="pagebreak"></div>
-      <p>The Honorary Secretary</p>
-      <p>Hong Kong Bar Association</p>
-      <p>LG2, High Court</p>
-      <p>38 Queensway</p>
-      <p>Hong Kong</p>
 
-      <LetterBody />
+      <div className="m-5">
+        <p>The Honorary Secretary</p>
+        <p>Hong Kong Bar Association</p>
+        <p>LG2, High Court</p>
+        <p>38 Queensway</p>
+        <p>Hong Kong</p>
+        <LetterBody />
+      </div>
+
       <div className="pagebreak"></div>
 
-      <p> The Secretary for Justice</p>
-      <p>Department of Justice</p>
-      <p>6/F, Main and East Wings</p>
-      <p>Justice Place</p>
-      <p>18 Lower Albert Road</p>
-
-      <LetterBody />
+      <div className="m-5">
+        <p> The Secretary for Justice</p>
+        <p>Department of Justice</p>
+        <p>6/F, Main and East Wings</p>
+        <p>Justice Place</p>
+        <p>18 Lower Albert Road</p>
+        <LetterBody />
+      </div>
+      
     </div>
   );
 }
