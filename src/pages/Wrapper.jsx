4457c31feb01b77d19d496db4baeb13b1db7dde9
Jasper Wong <wlcjasper@gmail.com>
2024-03-06 15:59:59 +0800

    complete the router with navigate

--- src/pages/Wrapper.jsx
+++ src/pages/Wrapper.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { useAppContext } from "../context.jsx";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Wrapper({ children }) {
   const { applicantAffidavit, idAffidavit, applicant, idDeponent } =
     useAppContext();
+
+    const navigate = useNavigate();
 
   return (
     <div className="min-h-100vh text-normal my-2 flex h-screen flex-col bg-slate-100 font-serif leading-relaxed text-black md:text-lg">
@@ -24,20 +26,24 @@ export default function Wrapper({ children }) {
             <Link to="info"> Enter Information</Link>
           </button>
 
-          <select className="border-2 border-white bg-slate-600 text-center md:border-0">
-            <option value="Info">Select Document</option>
-            <option value="Notice">
-              <Link to="notice">Notice of Motion</Link>
-            </option>
-            <option value="Applicant">
+          <select
+            className="border-2 border-white bg-slate-600 text-center md:border-0"
+            onChange={(e) => {
+              console.log(e.target.value);
+              navigate(`${e.target.value}`);
+            }}
+          >
+            <option value="info">Select Document</option>
+            <option value="notice">Notice of Motion</option>
+            <option value="applicant">
               {applicantAffidavit ? "Affidavit" : "Affirmation"} of{" "}
               {applicant === "APPLICANT" ? "the Applicant" : applicant}
             </option>
-            <option value="ID">
+            <option value="id">
               {idAffidavit ? "Affidavit" : "Affirmation"} of{" "}
               {idDeponent === "ID DEPONENT" ? "Identity" : idDeponent}
             </option>
-            <option value="Letters">Cover letters</option>
+            <option value="letters">Cover letters</option>
           </select>
         </nav>
       </header>
