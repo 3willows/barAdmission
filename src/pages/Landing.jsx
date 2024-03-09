ed07b7f91b32f3794f8ffe0aea56bd8a895d70dd
Jasper Wong <wlcjasper@gmail.com>
2024-03-09 22:10:53 +0800

    refactor as class component

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 
-export default function Landing({ setPage }) {
-  return (
-    <div className="m-4">
-      <article className="m-5 text-center">
-        <p className="m-5 text-center">
-          Generate court papers for Hong Kong barrister admission in one click.
-        </p>
-        <p className="m-5 text-center">
-          For pupils seeking admission in {new Date().getFullYear()}.
-        </p>
-      </article>
-    </div>
-  );
+// experiment with refactoring as class component
+
+export default class Landing extends React.Component{
+  render() {
+    return (
+      <div className="m-4">
+        <article className="m-5 text-center">
+          <p className="m-5 text-center">
+            Generate court papers for Hong Kong barrister admission in one
+            click.
+          </p>
+          <p className="m-5 text-center">
+            For pupils seeking admission in {new Date().getFullYear()}.
+          </p>
+        </article>
+      </div>
+    );
+  }
 }
