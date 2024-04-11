28feada9a2b2d7d1f5c727c86dff6dcf228cfecc
Jasper Wong <wlcjasper@gmail.com>
2024-04-11 17:55:21 +0800

    First click on textbox selects all text; other clicks normal.

--- src/components/DataInput.jsx
+++ src/components/DataInput.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAppContext } from "../context.jsx";
 
 export function DataInput({ prop }) {
   const Context = useAppContext();
-
   const dispatch = Context.dispatch;
-
   const value = Context[prop];
+
+  const [hasClicked, setHasClicked] = useState(false);
+
+  const handleInputClick = (e) => {
+    if (!hasClicked) {
+      e.target.select();
+      setHasClicked(true);
+    }
+  };
 
   return (
     <>
@@ -14,6 +21,7 @@ export function DataInput({ prop }) {
         type="text"
         className="bg-white text-black dark:bg-black dark:text-white"
         value={value}
+        onClick={handleInputClick}
         onChange={(e) => dispatch({ type: prop, payload: e.target.value })}
       ></input>
     </>
